7f4bbd1d5318e7abc538cc35ac7d3f4f670ce71a
jeebanbehera <143058499+jeebanbehera@users.noreply.github.com>
2025-01-07 11:49:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ main().then(()=>{
 async function main() {
   await mongoose.connect(dbUrl);
 
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+ 
 }
 
 app.set("view engine", "ejs")
